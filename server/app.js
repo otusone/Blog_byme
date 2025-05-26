ed0102c4804d5a9f0550f0099e8f8dcee602120d
ishita-072002 <ishita.otusone@gmail.com>
2025-05-26 12:16:14 +0530

    favicon changed

--- server/app.js
+++ server/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const helmet = require('helmet');
+//const helmet = require('helmet');
 const morgan = require('morgan');
 const errorHandler = require('./middleware/errorHandlers');
 const path = require('path');
@@ -9,24 +9,28 @@ const app = express();
 
 
 app.use(cors({
-    origin: 'http://localhost:5173',
-    credentials: true
+  origin: 'http://localhost:5173',
+  credentials: true
 }));
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 
-app.use(
-  helmet({
-    contentSecurityPolicy: {
-      directives: {
-        defaultSrc: ["'self'"],
-        imgSrc: ["'self'", "data:", "*"],
-      },
-    },
-  })
-);
+// app.use(
+//   helmet({
+//     contentSecurityPolicy: {
+//       directives: {
+//         defaultSrc: ["'self'"],
+//         scriptSrc: ["'self'", "https://cdn.tiny.cloud"],
+//         imgSrc: ["'self'", "data:", "*"],
+//         styleSrc: ["'self'", "'unsafe-inline'", "https://cdn.tiny.cloud"], 
+//         connectSrc: ["'self'", "https://cdn.tiny.cloud"], 
+//       },
+//     },
+//   })
+// );
+
 
 app.use(morgan('dev'));
 
@@ -38,7 +42,7 @@ app.use('/api/posts', require('./routes/postRoutes'));
 const buildPath = path.join(__dirname, '../screens', 'dist')
 app.use(express.static(buildPath))
 app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../screens', 'dist', "index.html"));
+  res.sendFile(path.join(__dirname, '../screens', 'dist', "index.html"));
 });
 
 
